2f4980802c1053cc41158102cbd5b0424d5cf787
BorisFR <mdpGH59>
2017-02-05 12:57:29 +0100

    updt readme

--- LaBuilderApp/LaBuilderAppPage.xaml.cs
+++ LaBuilderApp/LaBuilderAppPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Plugin.DeviceInfo;
+﻿using Plugin.AppInfo;
+using Plugin.DeviceInfo;
 using Xamarin.Forms;
 
 namespace LaBuilderApp
@@ -13,12 +14,14 @@ namespace LaBuilderApp
 
 		private void LoadInfo ()
 		{
-			var model = CrossDeviceInfo.Current.Model;
-			var platform = CrossDeviceInfo.Current.Platform;
-			var version = CrossDeviceInfo.Current.Version;
-			System.Diagnostics.Debug.WriteLine ($"Model: {model}");         // iPhone	/ Xamarin Android Player ('Phone' version)
-			System.Diagnostics.Debug.WriteLine ($"Platform: {platform}");   // iOS		// Android
-			System.Diagnostics.Debug.WriteLine ($"Version: {version}");     // 10.2		// 5.1
+			System.Diagnostics.Debug.WriteLine ("* Device info:");
+			System.Diagnostics.Debug.WriteLine ($"Model: {CrossDeviceInfo.Current.Model}");         // iPhone	/ Xamarin Android Player ('Phone' version)
+			System.Diagnostics.Debug.WriteLine ($"Platform: {CrossDeviceInfo.Current.Platform}");   // iOS		// Android
+			System.Diagnostics.Debug.WriteLine ($"Version: {CrossDeviceInfo.Current.Version}");     // 10.2		// 5.1
+			System.Diagnostics.Debug.WriteLine ("* App info:");
+			System.Diagnostics.Debug.WriteLine ($"Display: {CrossAppInfo.Current.DisplayName}");    // LaBuilderApp
+			System.Diagnostics.Debug.WriteLine ($"Package: {CrossAppInfo.Current.PackageName}");    // com.boris.labuilderapp
+			System.Diagnostics.Debug.WriteLine ($"Version: {CrossAppInfo.Current.Version}");        // 1.0
 		}
 
 	}
--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # LaBuilderApp
 
-packages utilisés:
-https://github.com/jamesmontemagno/DeviceInfoPlugin
-https://github.com/jamesmontemagno/MediaPlugin
-https://github.com/Aftnet/AppInfoPlugin
+packages utilisés
+-----------------
+Device Info, to display informations of the runing device [1].
+Media, to take pictures [2].
+App info, to display informations of the current app [3].
+
+
+[1]: https://github.com/jamesmontemagno/DeviceInfoPlugin
+[2]: https://github.com/jamesmontemagno/MediaPlugin
+[3]: https://github.com/Aftnet/AppInfoPlugin
