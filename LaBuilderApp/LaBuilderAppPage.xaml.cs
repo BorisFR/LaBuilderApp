f0316d4d714d1297b4901d3b7dd3140ee290c58a
BorisFR <mdpGH59>
2017-02-05 12:45:18 +0100

    add packages

--- LaBuilderApp/LaBuilderAppPage.xaml.cs
+++ LaBuilderApp/LaBuilderAppPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using Plugin.DeviceInfo;
+using Xamarin.Forms;
 
 namespace LaBuilderApp
 {
@@ -7,6 +8,18 @@ namespace LaBuilderApp
 		public LaBuilderAppPage ()
 		{
 			InitializeComponent ();
+			LoadInfo ();
 		}
+
+		private void LoadInfo ()
+		{
+			var model = CrossDeviceInfo.Current.Model;
+			var platform = CrossDeviceInfo.Current.Platform;
+			var version = CrossDeviceInfo.Current.Version;
+			System.Diagnostics.Debug.WriteLine ($"Model: {model}");         // iPhone	/ Xamarin Android Player ('Phone' version)
+			System.Diagnostics.Debug.WriteLine ($"Platform: {platform}");   // iOS		// Android
+			System.Diagnostics.Debug.WriteLine ($"Version: {version}");     // 10.2		// 5.1
+		}
+
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # LaBuilderApp
+
+packages utilisés:
+https://github.com/jamesmontemagno/DeviceInfoPlugin
+https://github.com/jamesmontemagno/MediaPlugin
+https://github.com/Aftnet/AppInfoPlugin
