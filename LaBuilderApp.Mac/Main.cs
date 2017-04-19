d9a67b079dc66b75f413944d3524dc9fc231091a
sfardoux <sfardoux@maxxing.com>
2017-04-19 09:23:15 +0200

    Add Mac project

---
+++ LaBuilderApp.Mac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace LaBuilderApp.Mac
+{
+	static class MainClass
+	{
+		static void Main (string [] args)
+		{
+			NSApplication.Init ();
+			NSApplication.Main (args);
+		}
+	}
+}
